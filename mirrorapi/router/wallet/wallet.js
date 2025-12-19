2e96cc0f932c69840872bdea59366a430e2fed3b
Aniket Bansode <aniketbansode268822@gmail.com>
2025-12-19 11:49:45 +0530

    init

--- mirrorapi/router/wallet/wallet.js
+++ mirrorapi/router/wallet/wallet.js
@@ -12,32 +12,32 @@ const endpoints = {
     '/get-upi': 'bef0c0a17a8e08250ed3f01d12a3d90a5513685f',
     '/credit-debit-income-to-user': 'd7dfaf86f2ab8c7013f268736ab747e07bd8558e',
     '/get-balance-test': '3d0426cb4eecf64d963966f6da3852804c20a951',
-        
+
 };
 
 wallet.post('/e1af0d84d643e7c955bee1ee6d03a8b9a88a07fd', authenticateJWT, logMiddleware, (req, res) => {
 
-	walletController.getWalletBalance(req.body,res)
-	    .then(data => res.json(data))
-	    .catch(error => {
+    walletController.getWalletBalance(req.body, res)
+
+        .catch(error => {
             console.error('Error requesting Get Wallet Balance:', error);
             res.status(500).json({ error: 'Internal Server Error' });
         });
 });
 wallet.get('/bef0c0a17a8e08250ed3f01d12a3d90a5513685f', authenticateJWT, logMiddleware, (req, res) => {
 
-	walletController.getUpiId(req,res)
-	    .then(data => res.json(data))
-	    .catch(error => {
+    walletController.getUpiId(req, res)
+
+        .catch(error => {
             console.error('Error requesting Get Upi:', error);
             res.status(500).json({ error: 'Internal Server Error' });
         });
 });
 
 wallet.post('/d7dfaf86f2ab8c7013f268736ab747e07bd8558e', authenticateJWT, logMiddleware, async (req, res) => {
-	
-    walletController.creditDebitIncomeToUser(req.body,res)
-        .then(data => res.json(data))
+
+    walletController.creditDebitIncomeToUser(req.body, res)
+
         .catch(error => {
             console.error('Error requesting Credit Debit income to user:', error);
             res.status(500).json({ error: 'Internal Server Error' });
@@ -46,9 +46,9 @@ wallet.post('/d7dfaf86f2ab8c7013f268736ab747e07bd8558e', authenticateJWT, logMid
 
 
 wallet.post('/3d0426cb4eecf64d963966f6da3852804c20a951', authenticateJWT, logMiddleware, async (req, res) => {
-	
-    walletController.getWalletBalanceTest(req.body,res)
-        .then(data => res.json(data))
+
+    walletController.getWalletBalanceTest(req.body, res)
+
         .catch(error => {
             console.error('Error requesting get balance Test:', error);
             res.status(500).json({ error: 'Internal Server Error' });
@@ -57,29 +57,29 @@ wallet.post('/3d0426cb4eecf64d963966f6da3852804c20a951', authenticateJWT, logMid
 
 
 
-wallet.post('/get-wallet-balance',(req, res) => {
+wallet.post('/get-wallet-balance', (req, res) => {
 
-	walletController.getWalletBalance(req.body,res).then(data => res.json(data));
+    walletController.getWalletBalance(req.body, res);
 });
-wallet.get('/get-upi',(req, res) => {
+wallet.get('/get-upi', (req, res) => {
 
-	walletController.getUpiId(req,res).then(data => res.json(data));
+    walletController.getUpiId(req, res);
 });
 
 wallet.post('/credit-debit-income-to-user', async (req, res) => {
-	
-    walletController.creditDebitIncomeToUser(req.body,res).then(data => res.json(data));
+
+    walletController.creditDebitIncomeToUser(req, res);
 });
 
 
 wallet.post('/bulk-credit-debit-income', async (req, res) => {
-	
-    walletController.bulkCreditDebitIncome(req.body,res).then(data => res.json(data));
+
+    walletController.bulkCreditDebitIncome(req.body, res);
 });
 
 wallet.post('/get-balance-test', async (req, res) => {
-	
-    walletController.getWalletBalanceTest(req.body,res).then(data => res.json(data));
+
+    walletController.getWalletBalanceTest(req.body, res);
 });
 
 
